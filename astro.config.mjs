e228b39a0229e4f73a8522b839b6ec596739d54a
weeix <weeix@users.noreply.github.com>
2025-01-19 04:53:22 +0000

    Added a Preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorial-weeix.netlify.app",
+  site: "https://astro-tutorial-weeix.netlify.app",
+  integrations: [preact()],
 });
